ba93bdfd46e9e11f2aa1644208b7b1eb4c146244
Sneruall <laurens@vr-house.nl>
2023-11-16 22:43:02 +0100

    styling page and header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -14,16 +14,16 @@ export function Header(props: HeaderProps) {
     <div className={`${centered ? 'text-center' : 'w-5/6 lg:w-3/5'}`}>
       {/* Title */}
       {title && (
-        <div className="text-3xl font-league tracking-tight md:text-5xl">
+        <div className="text-5xl font-league tracking-tight md:text-7xl">
           {title}
         </div>
       )}
       {/* Description */}
-      {description && (
-        <div className="mt-4 font-serif text-xl text-gray-600 md:text-2xl">
+      {/* {description && (
+        <div className="mt-4 font-playfair text-xl text-gray-900 md:text-2xl">
           <CustomPortableText value={description} />
         </div>
-      )}
+      )} */}
     </div>
   )
 }
