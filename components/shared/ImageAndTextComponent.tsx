1d2ec2a27642a2d62ff1dbfc6daf4065fded0162
Sneruall <laurens@vr-house.nl>
2023-11-16 22:25:27 +0100

    add mobile view for imageandtextcomponent

--- components/shared/ImageAndTextComponent.tsx
+++ components/shared/ImageAndTextComponent.tsx
@@ -11,14 +11,36 @@ export function ImageAndTextComponent(props: ImageAndTextComponentProps) {
   const { image, text } = props
   const imageUrl = urlForImage(image!)?.url() || ''
 
+  //   todo: checkbox in studio om ook voor desktop een 'full width' image te tonen? Of moet dat een ander 'hero' component zijn?
+
   return (
     <>
-      <div className="flex w-full h-[500px] justify-center space-x-10 lg:space-x-20">
+      {/* MOBILE VIEW */}
+      <div className="w-screen h-screen relative sm:hidden -mx-4">
+        {imageUrl && (
+          <Image
+            alt="todo alt"
+            fill
+            style={{
+              objectFit: 'cover',
+            }}
+            src={imageUrl}
+          />
+        )}
+        {/* todo: text kleur laten kiezen in sanity, zodat het leesbaar is op lichte en donkere achtergronden. Zelfde voor positie? Bedenken of het verschillende components worden of een met vars in de code */}
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <h2 className="text-2xl font-playfair text-center text-white">
+            {text}
+          </h2>
+        </div>
+      </div>
+      {/* DESKTOP VIEW */}
+      <div className="hidden sm:flex w-full h-[500px] justify-center space-x-10 lg:space-x-20">
         <div className="relative flex-1 max-w-lg">
           {imageUrl && (
             <Image
               className="rounded-lg"
-              alt="alt"
+              alt="todo alt"
               sizes="512px"
               fill
               style={{
@@ -31,6 +53,7 @@ export function ImageAndTextComponent(props: ImageAndTextComponentProps) {
         <div className="flex-1 m-auto max-w-md">
           <h2 className="text-2xl font-playfair text-center">{text}</h2>
         </div>
+        {/* Todo, call to actie toevoegen? Anders zo'n homepage waar je niet vanaf komt... */}
       </div>
     </>
   )
